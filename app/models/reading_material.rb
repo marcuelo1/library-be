162db0559af18f79964b928a899b0350dafafb03
Paul Brian Marcuelo <pmarcuelo@fullscale.io>
2023-04-05 23:17:31 +0800

    added search feature for all and each of the models

--- app/models/reading_material.rb
+++ app/models/reading_material.rb
@@ -3,6 +3,12 @@ class ReadingMaterial < ApplicationRecord
   validates :material, :title, presence: true
   after_create :set_description
 
+  include PgSearch::Model
+  pg_search_scope :search_all, against: :description,
+    using: {
+      tsearch: { prefix: true }
+    }
+
   def set_description
     if self.material_type == "Book"
       self.description = "#{self.title} #{self.material.author} #{self.material.isbn} #{self.material.dewey_decimal} #{self.material.issn}}"
@@ -17,5 +23,33 @@ class ReadingMaterial < ApplicationRecord
     end
 
     self.save
+  end
+
+  def self.search_books keyword
+    ReadingMaterial.where(material_type: "Book").search_all(keyword).collect { |reading_material| reading_material.material }
+  end
+
+  def self.search_magazines keyword
+    ReadingMaterial.where(material_type: "Magazine").search_all(keyword).collect { |reading_material| reading_material.material }
+  end
+
+  def self.search_e_books keyword
+    ReadingMaterial.where(material_type: "EBook").search_all(keyword).collect { |reading_material| reading_material.material }
+  end
+
+  def self.search_theses keyword
+    ReadingMaterial.where(material_type: "Thesis").search_all(keyword).collect { |reading_material| reading_material.material }
+  end
+
+  def self.search_newspapers keyword
+    ReadingMaterial.where(material_type: "Newspaper").search_all(keyword).collect { |reading_material| reading_material.material }
+  end
+  
+  def self.new_arrivals
+    ReadingMaterial.order(created_at: :desc).first(10).collect { |reading_material| reading_material.material }
+  end
+
+  def self.featured
+    ReadingMaterial.order("RANDOM()").first(10).collect { |reading_material| reading_material.material }
   end
 end
